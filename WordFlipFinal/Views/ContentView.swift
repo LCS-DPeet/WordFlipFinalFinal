d186fcc47d1ce3bae1e1312695082cffe52b39ae
Danika Peet <DPeet@lcs.on.ca>
2025-04-10 17:50:02 -0400

    added the buttons using code from my first try at this app just to set up the buttons

--- WordFlipFinal/Views/ContentView.swift
+++ WordFlipFinal/Views/ContentView.swift
@@ -10,28 +10,40 @@ import SwiftUI
 struct Card: Identifiable {
     let id = UUID()
     let cardImage: String
+    //Tracks if card is flipped
     var faceUp: Bool = false
-    var faceDown: Bool = false
+    //Tracks if card is matched
+    var cardMatched: Bool = false
 }
 
 struct ContentView: View {
+    
+    //MARK: Stored Properties:
+    
+    //Stores the cards in the game (empty array)
+    @State private var card: [Card] = []
+    //Stores the first card or nil (no card filpped)
+    @State private var firstFlippedIndex: Int? = nil
+    //The score for the game which can be updated later
+    @State private var score = 0
+    
     var body: some View {
         VStack {
-            
-            
-            Rectangle()
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+            HStack {
+              
+                Text("Score: \(score)")
+                    .font(.title3)
+                    .bold()
+                
+                Spacer()
+                
+                Button("New Game") {
+                    // add game in here
+                }
+                
+                
+                
+            }
             
         }
     }
