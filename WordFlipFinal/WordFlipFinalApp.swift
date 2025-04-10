580b5d666dd8eeaec0644e688ba67a57b4aa1c0e
Danika Peet <DPeet@lcs.on.ca>
2025-04-10 17:28:40 -0400

    Initial Commit

---
+++ WordFlipFinal/WordFlipFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordFlipFinalApp.swift
+//  WordFlipFinal
+//
+//  Created by Danika Peet on 2025-04-10.
+//
+
+import SwiftUI
+
+@main
+struct WordFlipFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
